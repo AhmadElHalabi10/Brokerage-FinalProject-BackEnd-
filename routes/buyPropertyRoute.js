5911dc2d69fda9e7967ba18d02ec72103581223a
AhmadElHalabi10 <ahmd.el.halabi.4@gmail.com>
2023-06-04 15:09:40 +0300

    Remove the uploadImage middleware

--- routes/buyPropertyRoute.js
+++ routes/buyPropertyRoute.js
@@ -1,6 +1,5 @@
 import express from "express";
 import controller from "../controllers/buyPropertyController.js";
-import uploadImage from "../middleware/image.js";
 const router = express.Router();
 import { isGeneralAdmin, verifyToken } from "../middleware/auth.js";
 router.get("/getOnlyFour", controller.getFourBuyPropertiesOnly);
@@ -8,13 +7,12 @@ router.get("/", controller.getAllBuyProperties);
 
 router.get("/byId/:id", controller.getBuyPropertyById);
 router.get("/title/:title", controller.getBuyPropertyByTitle);
-// router.get("/subcategory/:id", controller.getCategory);
-router.post("/", uploadImage, controller.addBuyProperty);
+router.post("/", controller.addBuyProperty);
 router.put(
   "/:id",
   verifyToken,
   isGeneralAdmin,
-  uploadImage,
+
   controller.editBuyProperty
 );
 router.delete(
--- routes/rentPropertyRoute.js
+++ routes/rentPropertyRoute.js
@@ -1,6 +1,5 @@
 import express from "express";
 import controller from "../controllers/rentPropertyController.js";
-import uploadImage from "../middleware/image.js";
 const router = express.Router();
 import { isGeneralAdmin, verifyToken } from "../middleware/auth.js";
 router.get("/getOnlyFour", controller.getFourRentPropertiesOnly);
@@ -8,15 +7,8 @@ router.get("/", controller.getAllRentProperties);
 
 router.get("/byId/:id", controller.getRentPropertyById);
 router.get("/title/:title", controller.getRentPropertyByTitle);
-// router.get("/subcategory/:id", controller.getCategory);
-router.post("/", uploadImage, controller.addRentProperty);
-router.put(
-  "/:id",
-  verifyToken,
-  isGeneralAdmin,
-  uploadImage,
-  controller.editRentProperty
-);
+router.post("/", controller.addRentProperty);
+router.put("/:id", verifyToken, isGeneralAdmin, controller.editRentProperty);
 router.delete(
   "/:id",
   verifyToken,
